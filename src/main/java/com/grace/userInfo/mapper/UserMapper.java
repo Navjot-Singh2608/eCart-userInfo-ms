3623ff5714e1fc1b3d46eba9c2c72f2d91ac94a5
navjotsingh2608 <navjot@argenelec.com>
2024-06-26 20:57:49 -0400

    update

---
+++ src/main/java/com/grace/userInfo/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.grace.userInfo.mapper;
+
+
+import com.grace.userInfo.dto.UserDTO;
+import com.grace.userInfo.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface UserMapper {
+
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+    User mapUserDTOToUser(UserDTO userDTO);
+    UserDTO mapUserToUserDTO(User user);
+
+}
---
+++ src/main/java/com/grace/userInfo/repo/UserRepo.java
@@ -0,0 +1,10 @@
+package com.grace.userInfo.repo;
+
+
+import com.grace.userInfo.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Integer> {
+}
